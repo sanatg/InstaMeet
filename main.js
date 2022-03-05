03e1cf9029a164fb9f369ea321f89a14737dafc6
Sanat Gupta <sanatasgsih@gmail.com>
2022-03-05 09:46:52 +0530

    something

--- main.js
+++ main.js
@@ -18,10 +18,16 @@ if (!firebase.apps.length) {
 const firestore = firebase.firestore();
 
 const servers = {
-  iceServers: [
-    {
-      urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
+  iceServers:[
+    {urls: ["turn:173.194.72.127:19305?transport=udp",
+       "turn:[2404:6800:4008:C01::7F]:19305?transport=udp",
+       "turn:173.194.72.127:443?transport=tcp",
+       "turn:[2404:6800:4008:C01::7F]:443?transport=tcp"
+       ],
+     username:"CKjCuLwFEgahxNRjuTAYzc/s6OMT",
+     credential:"u1SQDR/SQsPQIxXNWQT7czc/G4c="
     },
+    {urls:["stun:stun.l.google.com:19302"]}
   ],
   iceCandidatePoolSize: 10,
 };
